554bf4c06f7970d2c81750404113c431788cf782
phuocnguyen <nphuocnguyen2000@gmail.com>
2020-06-30 22:14:04 +0700

    del file build

--- public/js/menufix.js
+++
@@ -1,8 +0,0 @@
-// window.addEventListener('scroll',()=>{
-//     let headerMenu = document.getElementById('header-wrapper')
-//     headerMenu.classList.toggle('header-main__scroll', window.scrollY > 190)
-// })
-window.addEventListener('scroll',()=>{
-    let headerMenu = document.getElementById('menu-wrapper')
-    headerMenu.classList.toggle('menu-wrapper__scroll', window.scrollY > 200)
-})
